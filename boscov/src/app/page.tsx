24b56935e8c4e9e1e12f4a3e69ed77ecc7336cb5
florgust <luan.flor05@gmail.com>
2025-05-19 09:13:43 -0300

    main: criando casca do projeto

---
+++ boscov/src/app/page.tsx
@@ -0,0 +1,8 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-900">Olá, mundo com Tailwind!</h1>
+    </div>
+  );
+}
